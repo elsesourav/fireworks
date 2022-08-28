5ad2bc2acbcb402b61653451e89c547feed448e6
Sourav Barui <74421679+im-sourav@users.noreply.github.com>
2022-08-28 18:53:46 +0530

    update

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ let run = true;
 let fireworks = [];
 
 animation(5, true, () => {
-  if (random() < 0.3) {
+  if (random() < 0.1) {
     const v = scrHeight / 45;
     const vx = random(-0.5, 0.5);
     const vy = random(-v * 0.55, -v * 0.7); 
     const gra = v * 0.006;
     const r = random(2, 3);
-    fireworks.push(new Firework(random(scrWidth), scrHeight, r, vx, vy, gra))
+    fireworks.push(new Firework(random(scrWidth), scrHeight, r, vx, vy, gra));
   }
 })
 
